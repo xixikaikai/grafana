271dfd023b9598283bfe95f9a96dbfbf9ccb78f0
bergquist <carl.bergquist@gmail.com>
2017-09-21 17:02:40 +0200

    separate plugin impl and proto files

--- pkg/cmd/grafana-server/server.go
+++ pkg/cmd/grafana-server/server.go
@@ -17,6 +17,7 @@ import (
 	"github.com/grafana/grafana/pkg/metrics"
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/plugins"
+	tsdbplugins "github.com/grafana/grafana/pkg/plugins/backend"
 	"github.com/grafana/grafana/pkg/services/alerting"
 	"github.com/grafana/grafana/pkg/services/cleanup"
 	"github.com/grafana/grafana/pkg/services/eventpublisher"
@@ -25,7 +26,6 @@ import (
 	"github.com/grafana/grafana/pkg/setting"
 	"github.com/grafana/grafana/pkg/social"
 	"github.com/grafana/grafana/pkg/tracing"
-	tsdbplugins "github.com/grafana/grafana/pkg/tsdb/plugins"
 )
 
 func NewGrafanaServer() models.GrafanaServer {
